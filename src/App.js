18ec325cdd36f5fcaa07a956b3c687264fff692d
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-09-20 20:05:40 -0400

    component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import './index.scss';
 
+const Modal = ({ open, setOpen }) => (
+  <div className="overlay">
+    <div className="modal">
+      <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+        <title />
+        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      </svg>
+      <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='gif'/>
+    </div>
+  </div>
+)
+
 function App() {
   const [open, setOpen] = React.useState(false);
 
@@ -8,17 +20,7 @@ function App() {
   return (
     <div className="App">
       <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
-      { open && (
-        <div className="overlay">
-        <div className="modal">
-          <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='gif'/>
-        </div>
-      </div>
-      )}
+      {open && <Modal open={open} setOpen={setOpen}></Modal>}
     </div>
   );
 }
