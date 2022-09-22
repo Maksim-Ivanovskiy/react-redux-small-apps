df2352fa578f180d8501dc2277ac75992ef80704
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-09-21 21:27:27 -0400

    alert

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users')
+    .then(res => res.json())
+    .then(json => {
+      setUsers(json.data)
+    }).catch(err => {
+      console.warn(err);
+      alert('Error while receivend users data')
+    })
+  }, []);
   return (
     <div className="App">
       <Users />
